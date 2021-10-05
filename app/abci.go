b4465ebca22e6e89a1d35b0f08202dc251d12ba3
Thunnini <yunjh1994@gmail.com>
2021-10-05 20:57:58 +0900

    Forbid the resource intensive grpc query (/cosmos.staking.v1beta1.Query/ValidatorDelegations)

--- app/abci.go
+++ app/abci.go
@@ -11,6 +11,14 @@ import (
 // Query implements the ABCI interface. It delegates to CommitMultiStore if it
 // implements Queryable.
 func (app *OsmosisApp) Query(req abci.RequestQuery) (res abci.ResponseQuery) {
+	if req.Path == "/cosmos.staking.v1beta1.Query/ValidatorDelegations" {
+		return abci.ResponseQuery{
+			Code:      1,
+			Log:       "This query is too resource intensive. Please run your node",
+			Codespace: "forbidden",
+		}
+	}
+
 	if req.Path == "/simple-metric" || req.Path == "simple-metric" {
 		metricRes := app.SimpleMetrics.CalcAllAverageResponses()
 		jsonRes, err := json.Marshal(metricRes)
